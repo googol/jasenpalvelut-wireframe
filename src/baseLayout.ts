3dec442a35fdf7e17f667521396e7abff649a51b
Miika Hänninen <miika.hanninen@reaktor.com>
2019-10-20 19:54:07 +0300

    Flesh out looks

--- src/baseLayout.ts
+++ src/baseLayout.ts
@@ -5,12 +5,13 @@ export function baseLayout(body: readonly HtmlChild[]) {
     h('head', [
       h('title', ['Suomen partiolaisten jäsenpalvelut']),
       h('meta', { charset: 'utf-8' }),
-      h('meta', { name: 'theme-color', content: '#005073' }),
+      h('meta', { name: 'theme-color', content: '#253764' }),
       h('meta', {
         name: 'viewport',
         content: 'width=device-width, initial-scale=1.0',
       }),
-      h('link', { rel: 'stylesheet', href: './main.css' }),
+      h('link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=PT+Sans|Source+Sans+Pro:400,900&display=swap' }),
+      h('link', { rel: 'stylesheet', href: '/main.css' }),
     ]),
     h('body', body),
   ])
--- src/indexPage.ts
+++ src/indexPage.ts
@@ -1,5 +1,62 @@
 import { h } from './h'
 
 export const indexPage = [
-  h('h1', ['FOO BAR']),
+  header(),
+  membershipCard(),
+  menubar(),
+  notifications(),
+  events(),
+  scoutmedia(),
+  membershipBenefits(),
 ]
+
+function header() {
+  return h('section', { class: 'blue-background' }, [
+    h('h1', [h('img', { class: 'logo', src: 'https://partio.emmi.fi/customer/img/partio_logo_w.png' }), 'jäsenpalvelut'])
+  ])
+}
+
+function membershipCard() {
+  const member = {
+    name: 'Matti Meikäläinen',
+    memberNumber: '12345678',
+    localGroup: 'Metsänkävijät',
+    region: 'Pohjanmaan Partiolaiset',
+  }
+  return h('section', { class: 'membership-card blue-background' }, [
+    h('h2', [`Hei ${member.name}`]),
+    h('p', [member.localGroup]),
+    h('p', [member.region]),
+    h('p', [member.memberNumber]),
+  ])
+}
+
+function menubar() {
+  return h('nav', { class: 'menubar blue-background' }, [
+    h('p', ['navigation']),
+  ])
+}
+
+function notifications() {
+  return h('section', [
+    h('h2', ['Ilmoitukset']),
+  ])
+}
+
+function events() {
+  return h('section', [
+    h('h2', ['Tapahtumat']),
+  ])
+}
+
+function scoutmedia() {
+  return h('section', [
+    h('h2', ['Partiomedia']),
+  ])
+}
+
+function membershipBenefits() {
+  return h('section', [
+    h('h2', ['Jäsenedut']),
+  ])
+}
